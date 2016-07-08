709198d1042bb5b3b0cca23d0a53ed5a74bb77d9
Keith Ramey <keith.ramey@gmail.com>
2016-07-08 15:44:10 +0000

    Added functions to add, subtract, and scale a vector.

--- vector.py
+++ vector.py
@@ -1,3 +1,46 @@
+import operator
+
+class Vector(object):
+    def __init__(self, coordinates):
+        try:
+            if not coordinates:
+                raise ValueError
+            self.coordinates = tuple(coordinates)
+            self.dimension = len(coordinates)
+
+        except ValueError:
+            raise ValueError('The coordinates must be nonempty')
+
+        except TypeError:
+            raise TypeError('The coordinates must be an iterable')
 
 
-class Vector(Object):
+    def __str__(self):
+        return 'Vector: {}'.format(self.coordinates)
+
+
+    def __eq__(self, v):
+        return self.coordinates == v.coordinates
+
+
+    def add(self, v):
+        return Vector(map(operator.add, self.coordinates, v.coordinates))
+
+
+    def subtract(self, v):
+        return Vector(map(operator.sub, self.coordinates, v.coordinates))
+
+    def scale(self, scalar):
+        self.coordinates = [x*scalar for x in self.coordinates]
+
+
+my_vector = Vector((1,2,3))
+print my_vector
+my_vector2 = Vector((1,2,3))
+my_vector3 = Vector((-1,2,3))
+print my_vector == my_vector2
+print my_vector == my_vector3
+print 'my_vector + my_vector2' ,my_vector.add(my_vector2)
+print 'my_vector - my_vector3' ,my_vector.subtract(my_vector3)
+my_vector2.scale(3)
+print 'my_vector2 scaled by 3' ,my_vector2
