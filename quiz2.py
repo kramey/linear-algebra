5df5e443dec22d4563e1b364343ef1ec32d0d0fe
Keith Ramey <keith.ramey@gmail.com>
2016-07-08 21:05:55 +0000

    Added new source file for quiz on magnitudes and normalization of vectors.

---
+++ quiz2.py
@@ -0,0 +1,11 @@
+from vector import *
+
+v1 = Vector([-0.221,7.437])
+v2 = Vector([8.813,-1.331,-6.247])
+v3 = Vector([5.581,-2.136])
+v4 = Vector([1.996,3.108,-4.554])
+
+print v1, 'magnitude is', v1.magnitude()
+print v2, 'magnitude is', v2.magnitude()
+print v3, 'normalized is', v3.normalize()
+print v4, 'normalized is', v4.normalize()
